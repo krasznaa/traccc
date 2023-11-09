bc914b36ed21d9b0992f728746ed72e35f1e3454
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2023-11-09 10:48:15 +0100

    Start with a vecmem::edm based pixel cell EDM.

---
+++ core/include/traccc/edm/pixel_cell_container.hpp
@@ -0,0 +1,46 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2022-2023 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#pragma once
+
+// Project include(s).
+#include "traccc/definitions/common.hpp"
+#include "traccc/definitions/primitives.hpp"
+
+// VecMem include(s).
+#include "vecmem/edm/accessor.hpp"
+#include "vecmem/edm/container.hpp"
+
+namespace traccc::edm {
+
+/// Pixel cell (SoA) container
+struct pixel_cell_container
+    : vecmem::edm::container<vecmem::edm::schema<
+          vecmem::edm::type::vector<channel_id>,
+          vecmem::edm::type::vector<channel_id>,
+          vecmem::edm::type::vector<scalar>, vecmem::edm::type::vector<scalar>,
+          vecmem::edm::type::vector<unsigned int> > > {
+
+    /// @name Accessors for the individual container variables
+    /// @{
+
+    /// First (x) channel identifier of the cell
+    using channel0 = vecmem::edm::accessor<0, schema>;
+    /// Second (y) channel identifier of the cell
+    using channel1 = vecmem::edm::accessor<1, schema>;
+    /// Activation / signal strength of the cell
+    using activation = vecmem::edm::accessor<2, schema>;
+    /// Time of the cell signal
+    using time = vecmem::edm::accessor<3, schema>;
+    /// Index of the pixel module that the pixel cell belongs to
+    using module_index = vecmem::edm::accessor<4, schema>;
+
+    /// @}
+
+};  // struct cell_container
+
+}  // namespace traccc::edm
