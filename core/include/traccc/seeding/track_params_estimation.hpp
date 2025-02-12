ec9dba05c404926847d55770dff50fe6b775cf25
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2025-02-12 16:43:17 +0100

    Introduced SoA spacepoints in traccc::core.

--- core/include/traccc/seeding/track_params_estimation.hpp
+++ core/include/traccc/seeding/track_params_estimation.hpp
@@ -1,6 +1,6 @@
 /** TRACCC library, part of the ACTS project (R&D line)
  *
- * (c) 2021-2024 CERN for the benefit of the ACTS project
+ * (c) 2021-2025 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -8,8 +8,9 @@
 #pragma once
 
 // Library include(s).
-#include "traccc/edm/seed.hpp"
-#include "traccc/edm/spacepoint.hpp"
+#include "traccc/edm/measurement.hpp"
+#include "traccc/edm/seed_collection.hpp"
+#include "traccc/edm/spacepoint_collection.hpp"
 #include "traccc/edm/track_parameters.hpp"
 #include "traccc/utils/algorithm.hpp"
 
@@ -19,7 +20,7 @@
 // System include(s).
 #include <functional>
 
-namespace traccc {
+namespace traccc::host {
 
 /// Track parameter estimation algorithm
 ///
@@ -27,8 +28,9 @@ namespace traccc {
 ///
 class track_params_estimation
     : public algorithm<bound_track_parameters_collection_types::host(
-          const spacepoint_collection_types::host&,
-          const seed_collection_types::host&, const vector3&,
+          const measurement_collection_types::const_view&,
+          const edm::spacepoint_collection::const_view&,
+          const edm::seed_collection::const_view&, const vector3&,
           const std::array<traccc::scalar, traccc::e_bound_size>&)> {
 
     public:
@@ -39,6 +41,7 @@ class track_params_estimation
 
     /// Callable operator for track_params_esitmation
     ///
+    /// @param measurements All measurements of the event
     /// @param spacepoints All spacepoints of the event
     /// @param seeds The reconstructed track seeds of the event
     /// @param bfield (Temporary) Magnetic field vector
@@ -47,8 +50,9 @@ class track_params_estimation
     /// @return A vector of bound track parameters
     ///
     output_type operator()(
-        const spacepoint_collection_types::host& spacepoints,
-        const seed_collection_types::host& seeds, const vector3& bfield,
+        const measurement_collection_types::const_view& measurements,
+        const edm::spacepoint_collection::const_view& spacepoints,
+        const edm::seed_collection::const_view& seeds, const vector3& bfield,
         const std::array<traccc::scalar, traccc::e_bound_size>& stddev = {
             0.02f * traccc::unit<traccc::scalar>::mm,
             0.03f * traccc::unit<traccc::scalar>::mm,
@@ -63,4 +67,4 @@ class track_params_estimation
 
 };  // class track_params_estimation
 
-}  // namespace traccc
+}  // namespace traccc::host
