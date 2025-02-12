24375bb00e6871ececb7a7a74b081449d60c2a91
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2025-02-12 16:43:17 +0100

    Introduced SoA spacepoints in traccc::core.

--- core/src/seeding/seeding_algorithm.cpp
+++ core/src/seeding/seeding_algorithm.cpp
@@ -1,6 +1,6 @@
 /** TRACCC library, part of the ACTS project (R&D line)
  *
- * (c) 2021-2022 CERN for the benefit of the ACTS project
+ * (c) 2021-2025 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -8,25 +8,19 @@
 // Library include(s).
 #include "traccc/seeding/seeding_algorithm.hpp"
 
-#include "traccc/seeding/detail/seeding_config.hpp"
-
-// System include(s).
-#include <cmath>
-#include <iostream>
-
-namespace traccc {
+namespace traccc::host {
 
 seeding_algorithm::seeding_algorithm(const seedfinder_config& finder_config,
                                      const spacepoint_grid_config& grid_config,
                                      const seedfilter_config& filter_config,
                                      vecmem::memory_resource& mr)
-    : m_spacepoint_binning(finder_config, grid_config, mr),
-      m_seed_finding(finder_config, filter_config) {}
+    : m_binning{finder_config, grid_config, mr},
+      m_finding{finder_config, filter_config, mr} {}
 
 seeding_algorithm::output_type seeding_algorithm::operator()(
-    const spacepoint_collection_types::host& spacepoints) const {
+    const edm::spacepoint_collection::const_view& spacepoints) const {
 
-    return m_seed_finding(spacepoints, m_spacepoint_binning(spacepoints));
+    return m_finding(spacepoints, m_binning(spacepoints));
 }
 
-}  // namespace traccc
+}  // namespace traccc::host
