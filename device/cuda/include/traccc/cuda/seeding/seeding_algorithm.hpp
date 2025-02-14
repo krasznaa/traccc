013c513acf4a284f414ff4becfabe4e2aacd422e
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2025-02-14 18:27:00 +0100

    Introduced SoA spacepoints in traccc::device_common and traccc::cuda.

--- device/cuda/include/traccc/cuda/seeding/seeding_algorithm.hpp
+++ device/cuda/include/traccc/cuda/seeding/seeding_algorithm.hpp
@@ -1,6 +1,6 @@
 /** TRACCC library, part of the ACTS project (R&D line)
  *
- * (c) 2021-2023 CERN for the benefit of the ACTS project
+ * (c) 2021-2025 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -8,20 +8,20 @@
 #pragma once
 
 // Library include(s).
-#include "traccc/cuda/seeding/seed_finding.hpp"
-#include "traccc/cuda/seeding/spacepoint_binning.hpp"
 #include "traccc/cuda/utils/stream.hpp"
 
 // Project include(s).
-#include "traccc/edm/seed.hpp"
-#include "traccc/edm/spacepoint.hpp"
+#include "traccc/edm/seed_collection.hpp"
+#include "traccc/edm/spacepoint_collection.hpp"
+#include "traccc/seeding/detail/seeding_config.hpp"
 #include "traccc/utils/algorithm.hpp"
+#include "traccc/utils/memory_resource.hpp"
 
 // VecMem include(s).
 #include <vecmem/utils/copy.hpp>
 
-// traccc library include(s).
-#include "traccc/utils/memory_resource.hpp"
+// System include(s).
+#include <memory>
 
 namespace traccc::cuda {
 
@@ -30,8 +30,8 @@ namespace traccc::cuda {
 /// This algorithm returns a buffer which is not necessarily filled yet. A
 /// synchronisation statement is required before destroying this buffer.
 ///
-class seeding_algorithm : public algorithm<seed_collection_types::buffer(
-                              const spacepoint_collection_types::const_view&)> {
+class seeding_algorithm : public algorithm<edm::seed_collection::buffer(
+                              const edm::spacepoint_collection::const_view&)> {
 
     public:
     /// Constructor for the seed finding algorithm
@@ -47,20 +47,22 @@ class seeding_algorithm : public algorithm<seed_collection_types::buffer(
                       const seedfilter_config& filter_config,
                       const traccc::memory_resource& mr, vecmem::copy& copy,
                       stream& str);
+    /// Destructor
+    ~seeding_algorithm() override;
 
     /// Operator executing the algorithm.
     ///
-    /// @param spacepoints_view is a view of all spacepoints in the event
+    /// @param spacepoints is a view of all spacepoints in the event
     /// @return the buffer of track seeds reconstructed from the spacepoints
     ///
-    output_type operator()(const spacepoint_collection_types::const_view&
-                               spacepoints_view) const override;
+    output_type operator()(const edm::spacepoint_collection::const_view&
+                               spacepoints) const override;
 
     private:
-    /// Sub-algorithm performing the spacepoint binning
-    spacepoint_binning m_spacepoint_binning;
-    /// Sub-algorithm performing the seed finding
-    seed_finding m_seed_finding;
+    /// Internal data for the algorithm
+    struct impl;
+    /// Pointer to the internal data
+    std::unique_ptr<impl> m_impl;
 
 };  // class seeding_algorithm
 
--- device/cuda/src/seeding/seeding_algorithm.cpp
+++ device/cuda/src/seeding/seeding_algorithm.cpp
@@ -8,6 +8,9 @@
 // Library include(s).
 #include "traccc/cuda/seeding/seeding_algorithm.hpp"
 
+#include "seed_finding.hpp"
+#include "spacepoint_binning.hpp"
+
 // Project include(s).
 #include "traccc/seeding/detail/seeding_config.hpp"
 
@@ -16,19 +19,29 @@
 
 namespace traccc::cuda {
 
+struct seeding_algorithm::impl {
+    /// Sub-algorithm performing the spacepoint binning
+    spacepoint_binning m_binning;
+    /// Sub-algorithm performing the seed finding
+    seed_finding m_finding;
+};
+
 seeding_algorithm::seeding_algorithm(const seedfinder_config& finder_config,
                                      const spacepoint_grid_config& grid_config,
                                      const seedfilter_config& filter_config,
                                      const traccc::memory_resource& mr,
                                      vecmem::copy& copy, stream& str)
-    : m_spacepoint_binning(finder_config, grid_config, mr, copy, str),
-      m_seed_finding(finder_config, filter_config, mr, copy, str) {}
+    : m_impl{std::make_unique<impl>(
+          spacepoint_binning{finder_config, grid_config, mr, copy, str},
+          seed_finding{finder_config, filter_config, mr, copy, str})} {}
+
+seeding_algorithm::~seeding_algorithm() = default;
 
 seeding_algorithm::output_type seeding_algorithm::operator()(
-    const spacepoint_collection_types::const_view& spacepoints_view) const {
+    const edm::spacepoint_collection::const_view& spacepoints_view) const {
 
-    return m_seed_finding(spacepoints_view,
-                          m_spacepoint_binning(spacepoints_view));
+    return m_impl->m_finding(spacepoints_view,
+                             m_impl->m_binning(spacepoints_view));
 }
 
 }  // namespace traccc::cuda
