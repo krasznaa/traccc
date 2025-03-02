44e77779a0a452afe0c10d02b832f8436a80445c
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2025-03-02 23:34:09 +0100

    Migrated the benchmarks and remaining examples and tests to edm::track_candidate_collection.

--- tests/cpu/test_ambiguity_resolution.cpp
+++ tests/cpu/test_ambiguity_resolution.cpp
@@ -23,30 +23,34 @@ using namespace traccc;
 
 namespace {
 vecmem::host_memory_resource host_mr;
-traccc::memory_resource mr{host_mr, &host_mr};
-
 }  // namespace
 
-void fill_pattern(track_candidate_container_types::host& track_candidates,
-                  const std::size_t idx, const traccc::scalar chi2,
-                  const std::vector<std::size_t>& pattern) {
+void fill_pattern(
+    edm::track_candidate_collection<default_algebra>::host& track_candidates,
+    measurement_collection_types::host& measurements, const std::size_t idx,
+    const traccc::scalar chi2, const std::vector<std::size_t>& pattern) {
 
-    track_candidates.at(idx).header.trk_quality.chi2 = chi2;
+    track_candidates.at(idx).chi2() = chi2;
 
-    auto& cands = track_candidates.at(idx).items;
     for (const auto& meas_id : pattern) {
-        cands.push_back({});
-        cands.back().measurement_id = meas_id;
+        measurements.emplace_back();
+        measurements.back().measurement_id = meas_id;
+        track_candidates.at(idx).measurement_indices().push_back(
+            static_cast<unsigned int>(measurements.size() - 1));
     }
 }
 
 std::vector<std::size_t> get_pattern(
-    track_candidate_container_types::host& track_candidates,
+    const edm::track_candidate_collection<default_algebra>::host&
+        track_candidates,
+    const measurement_collection_types::host& measurements,
     const std::size_t idx) {
     std::vector<std::size_t> ret;
-    auto& cands = track_candidates.at(idx).items;
-    for (const auto& cand : cands) {
-        ret.push_back(cand.measurement_id);
+    // A const reference would be fine here. But GCC fears that that would lead
+    // to a dangling reference...
+    const auto meas_indices = track_candidates.at(idx).measurement_indices();
+    for (unsigned int meas_idx : meas_indices) {
+        ret.push_back(measurements.at(meas_idx).measurement_id);
     }
 
     return ret;
@@ -54,37 +58,40 @@ std::vector<std::size_t> get_pattern(
 
 TEST(AmbiguitySolverTests, GreedyResolverTest0) {
 
-    track_candidate_container_types::host trk_cands;
+    measurement_collection_types::host measurements{&host_mr};
+    edm::track_candidate_collection<default_algebra>::host trk_cands{host_mr};
 
     trk_cands.resize(3u);
-    fill_pattern(trk_cands, 0, 8.3f, {1, 3, 5, 11});
-    fill_pattern(trk_cands, 1, 2.2f, {6, 7, 8, 9, 10, 12});
-    fill_pattern(trk_cands, 2, 3.7f, {2, 4, 13});
+    fill_pattern(trk_cands, measurements, 0, 8.3f, {1, 3, 5, 11});
+    fill_pattern(trk_cands, measurements, 1, 2.2f, {6, 7, 8, 9, 10, 12});
+    fill_pattern(trk_cands, measurements, 2, 3.7f, {2, 4, 13});
 
     traccc::host::greedy_ambiguity_resolution_algorithm::config_type
         resolution_config;
 
     traccc::host::greedy_ambiguity_resolution_algorithm resolution_alg(
-        resolution_config, mr);
+        resolution_config, host_mr);
     {
         resolution_alg.get_config().min_meas_per_track = 3;
-        auto res_trk_cands = resolution_alg(traccc::get_data(trk_cands));
+        auto res_trk_cands = resolution_alg(vecmem::get_data(trk_cands),
+                                            vecmem::get_data(measurements));
         // All tracks are accepted as they have more than three measurements
         ASSERT_EQ(res_trk_cands.size(), 3u);
-        ASSERT_EQ(get_pattern(res_trk_cands, 0),
+        ASSERT_EQ(get_pattern(res_trk_cands, measurements, 0),
                   std::vector<std::size_t>({1, 3, 5, 11}));
-        ASSERT_EQ(get_pattern(res_trk_cands, 1),
+        ASSERT_EQ(get_pattern(res_trk_cands, measurements, 1),
                   std::vector<std::size_t>({6, 7, 8, 9, 10, 12}));
-        ASSERT_EQ(get_pattern(res_trk_cands, 2),
+        ASSERT_EQ(get_pattern(res_trk_cands, measurements, 2),
                   std::vector<std::size_t>({2, 4, 13}));
     }
 
     {
         resolution_alg.get_config().min_meas_per_track = 5;
-        auto res_trk_cands = resolution_alg(traccc::get_data(trk_cands));
+        auto res_trk_cands = resolution_alg(vecmem::get_data(trk_cands),
+                                            vecmem::get_data(measurements));
         // Only the second track with six measurements is accepted
         ASSERT_EQ(res_trk_cands.size(), 1u);
-        ASSERT_EQ(get_pattern(res_trk_cands, 0),
+        ASSERT_EQ(get_pattern(res_trk_cands, measurements, 0),
                   std::vector<std::size_t>({6, 7, 8, 9, 10, 12}));
     }
 
@@ -96,10 +103,10 @@ TEST(AmbiguitySolverTests, GreedyResolverTest0) {
         traccc::legacy::greedy_ambiguity_resolution_algorithm::config_t
             legacy_cfg;
         traccc::legacy::greedy_ambiguity_resolution_algorithm
-            legacy_resolution_alg(legacy_cfg);
+            legacy_resolution_alg(legacy_cfg, host_mr);
 
         legacy_resolution_alg.get_config().n_measurements_min = 3;
-        auto res_trk_cands = legacy_resolution_alg(trk_cands);
+        auto res_trk_cands = legacy_resolution_alg(trk_cands, measurements);
         ASSERT_EQ(res_trk_cands.size(), 3u);
     }
 
@@ -107,36 +114,38 @@ TEST(AmbiguitySolverTests, GreedyResolverTest0) {
         traccc::legacy::greedy_ambiguity_resolution_algorithm::config_t
             legacy_cfg;
         traccc::legacy::greedy_ambiguity_resolution_algorithm
-            legacy_resolution_alg(legacy_cfg);
+            legacy_resolution_alg(legacy_cfg, host_mr);
 
         legacy_resolution_alg.get_config().n_measurements_min = 5;
-        auto res_trk_cands = legacy_resolution_alg(trk_cands);
+        auto res_trk_cands = legacy_resolution_alg(trk_cands, measurements);
         ASSERT_EQ(res_trk_cands.size(), 1u);
-        ASSERT_EQ(get_pattern(res_trk_cands, 0),
+        ASSERT_EQ(get_pattern(res_trk_cands, measurements, 0),
                   std::vector<std::size_t>({6, 7, 8, 9, 10, 12}));
     }
 }
 
 TEST(AmbiguitySolverTests, GreedyResolverTest1) {
 
-    track_candidate_container_types::host trk_cands;
+    measurement_collection_types::host measurements{&host_mr};
+    edm::track_candidate_collection<default_algebra>::host trk_cands{host_mr};
 
     trk_cands.resize(2u);
-    fill_pattern(trk_cands, 0, 4.3f, {1, 3, 5, 11, 14, 16, 18});
-    fill_pattern(trk_cands, 1, 1.2f, {3, 5, 6, 13});
+    fill_pattern(trk_cands, measurements, 0, 4.3f, {1, 3, 5, 11, 14, 16, 18});
+    fill_pattern(trk_cands, measurements, 1, 1.2f, {3, 5, 6, 13});
 
     traccc::host::greedy_ambiguity_resolution_algorithm::config_type
         resolution_config;
     traccc::host::greedy_ambiguity_resolution_algorithm resolution_alg(
-        resolution_config, mr);
+        resolution_config, host_mr);
     {
-        auto res_trk_cands = resolution_alg(traccc::get_data(trk_cands));
+        auto res_trk_cands = resolution_alg(vecmem::get_data(trk_cands),
+                                            vecmem::get_data(measurements));
         ASSERT_EQ(res_trk_cands.size(), 1u);
 
         // The first track is selected over the second one as its relative
         // shared measurement (2/7) is lower than the one of the second track
         // (2/4)
-        ASSERT_EQ(get_pattern(res_trk_cands, 0),
+        ASSERT_EQ(get_pattern(res_trk_cands, measurements, 0),
                   std::vector<std::size_t>({1, 3, 5, 11, 14, 16, 18}));
     }
 
@@ -148,80 +157,85 @@ TEST(AmbiguitySolverTests, GreedyResolverTest1) {
         traccc::legacy::greedy_ambiguity_resolution_algorithm::config_t
             legacy_cfg;
         traccc::legacy::greedy_ambiguity_resolution_algorithm
-            legacy_resolution_alg(legacy_cfg);
+            legacy_resolution_alg(legacy_cfg, host_mr);
 
-        auto res_trk_cands = legacy_resolution_alg(trk_cands);
+        auto res_trk_cands = legacy_resolution_alg(trk_cands, measurements);
         ASSERT_EQ(res_trk_cands.size(), 1u);
 
         // The first track is selected over the second one as its relative
         // shared measurement (2/7) is lower than the one of the second track
         // (2/4)
-        ASSERT_EQ(get_pattern(res_trk_cands, 0),
+        ASSERT_EQ(get_pattern(res_trk_cands, measurements, 0),
                   std::vector<std::size_t>({1, 3, 5, 11, 14, 16, 18}));
     }
 }
 
 TEST(AmbiguitySolverTests, GreedyResolverTest2) {
 
-    track_candidate_container_types::host trk_cands;
+    measurement_collection_types::host measurements{&host_mr};
+    edm::track_candidate_collection<default_algebra>::host trk_cands{host_mr};
 
     trk_cands.resize(2u);
-    fill_pattern(trk_cands, 0, 4.3f, {1, 3, 5, 11});
-    fill_pattern(trk_cands, 1, 1.2f, {3, 5, 6, 13});
+    fill_pattern(trk_cands, measurements, 0, 4.3f, {1, 3, 5, 11});
+    fill_pattern(trk_cands, measurements, 1, 1.2f, {3, 5, 6, 13});
 
     traccc::host::greedy_ambiguity_resolution_algorithm::config_type
         resolution_config;
     traccc::host::greedy_ambiguity_resolution_algorithm resolution_alg(
-        resolution_config, mr);
+        resolution_config, host_mr);
     {
-        auto res_trk_cands = resolution_alg(traccc::get_data(trk_cands));
+        auto res_trk_cands = resolution_alg(vecmem::get_data(trk_cands),
+                                            vecmem::get_data(measurements));
         ASSERT_EQ(res_trk_cands.size(), 1u);
 
         // The second track is selected over the first one as their relative
         // shared measurement (2/4) is the same but its chi square is smaller
-        ASSERT_EQ(get_pattern(res_trk_cands, 0),
+        ASSERT_EQ(get_pattern(res_trk_cands, measurements, 0),
                   std::vector<std::size_t>({3, 5, 6, 13}));
     }
 }
 
 TEST(AmbiguitySolverTests, GreedyResolverTest3) {
 
-    track_candidate_container_types::host trk_cands;
+    measurement_collection_types::host measurements{&host_mr};
+    edm::track_candidate_collection<default_algebra>::host trk_cands{host_mr};
+
     trk_cands.resize(6u);
-    fill_pattern(trk_cands, 0, 5.3f, {1, 3, 5, 11});
-    fill_pattern(trk_cands, 1, 2.4f, {2, 6});
-    fill_pattern(trk_cands, 2, 2.5f, {3, 6, 12, 14, 19, 21});
-    fill_pattern(trk_cands, 3, 13.3f, {2, 7, 11, 13, 16});
-    fill_pattern(trk_cands, 4, 4.1f, {1, 7, 8});
-    fill_pattern(trk_cands, 5, 1.1f, {1, 3, 11, 22});
+    fill_pattern(trk_cands, measurements, 0, 5.3f, {1, 3, 5, 11});
+    fill_pattern(trk_cands, measurements, 1, 2.4f, {2, 6});
+    fill_pattern(trk_cands, measurements, 2, 2.5f, {3, 6, 12, 14, 19, 21});
+    fill_pattern(trk_cands, measurements, 3, 13.3f, {2, 7, 11, 13, 16});
+    fill_pattern(trk_cands, measurements, 4, 4.1f, {1, 7, 8});
+    fill_pattern(trk_cands, measurements, 5, 1.1f, {1, 3, 11, 22});
 
     traccc::host::greedy_ambiguity_resolution_algorithm::config_type
         resolution_config;
     traccc::host::greedy_ambiguity_resolution_algorithm resolution_alg(
-        resolution_config, mr);
+        resolution_config, host_mr);
 
     {
-        auto res_trk_cands = resolution_alg(traccc::get_data(trk_cands));
+        auto res_trk_cands = resolution_alg(vecmem::get_data(trk_cands),
+                                            vecmem::get_data(measurements));
         ASSERT_EQ(res_trk_cands.size(), 2u);
 
-        ASSERT_EQ(get_pattern(res_trk_cands, 0),
+        ASSERT_EQ(get_pattern(res_trk_cands, measurements, 0),
                   std::vector<std::size_t>({3, 6, 12, 14, 19, 21}));
-        ASSERT_EQ(get_pattern(res_trk_cands, 1),
+        ASSERT_EQ(get_pattern(res_trk_cands, measurements, 1),
                   std::vector<std::size_t>({2, 7, 11, 13, 16}));
     }
 
     // Legacy algorithm
     traccc::legacy::greedy_ambiguity_resolution_algorithm::config_t legacy_cfg;
     traccc::legacy::greedy_ambiguity_resolution_algorithm legacy_resolution_alg(
-        legacy_cfg);
+        legacy_cfg, host_mr);
 
     {
-        auto res_trk_cands = legacy_resolution_alg(trk_cands);
+        auto res_trk_cands = legacy_resolution_alg(trk_cands, measurements);
         ASSERT_EQ(res_trk_cands.size(), 2u);
 
-        ASSERT_EQ(get_pattern(res_trk_cands, 0),
+        ASSERT_EQ(get_pattern(res_trk_cands, measurements, 0),
                   std::vector<std::size_t>({3, 6, 12, 14, 19, 21}));
-        ASSERT_EQ(get_pattern(res_trk_cands, 1),
+        ASSERT_EQ(get_pattern(res_trk_cands, measurements, 1),
                   std::vector<std::size_t>({2, 7, 11, 13, 16}));
     }
 }
@@ -231,7 +245,8 @@ TEST(AmbiguitySolverTests, GreedyResolverTest4) {
 
     std::size_t n_tracks = 10000u;
 
-    track_candidate_container_types::host trk_cands;
+    measurement_collection_types::host measurements{&host_mr};
+    edm::track_candidate_collection<default_algebra>::host trk_cands{host_mr};
     trk_cands.resize(n_tracks);
     std::mt19937 gen(42);
 
@@ -264,17 +279,18 @@ TEST(AmbiguitySolverTests, GreedyResolverTest4) {
         ASSERT_EQ(pattern.size(), track_length);
 
         // Fill the pattern
-        fill_pattern(trk_cands, i, chi2, pattern);
+        fill_pattern(trk_cands, measurements, i, chi2, pattern);
     }
 
     traccc::host::greedy_ambiguity_resolution_algorithm::config_type
         resolution_config;
     traccc::host::greedy_ambiguity_resolution_algorithm resolution_alg(
-        resolution_config, mr);
+        resolution_config, host_mr);
 
     auto start_new = std::chrono::high_resolution_clock::now();
 
-    auto res_trk_cands = resolution_alg(traccc::get_data(trk_cands));
+    auto res_trk_cands = resolution_alg(vecmem::get_data(trk_cands),
+                                        vecmem::get_data(measurements));
 
     auto end_new = std::chrono::high_resolution_clock::now();
     auto duration_new = std::chrono::duration_cast<std::chrono::milliseconds>(
@@ -285,11 +301,11 @@ TEST(AmbiguitySolverTests, GreedyResolverTest4) {
     // Legacy algorithm
     traccc::legacy::greedy_ambiguity_resolution_algorithm::config_t legacy_cfg;
     traccc::legacy::greedy_ambiguity_resolution_algorithm legacy_resolution_alg(
-        legacy_cfg);
+        legacy_cfg, host_mr);
 
     auto start_legacy = std::chrono::high_resolution_clock::now();
 
-    auto legacy_res_trk_cands = legacy_resolution_alg(trk_cands);
+    auto legacy_res_trk_cands = legacy_resolution_alg(trk_cands, measurements);
 
     auto end_legacy = std::chrono::high_resolution_clock::now();
     auto duration_legacy =
@@ -301,8 +317,6 @@ TEST(AmbiguitySolverTests, GreedyResolverTest4) {
     std::size_t n_res_tracks = res_trk_cands.size();
     ASSERT_EQ(n_res_tracks, legacy_res_trk_cands.size());
     for (std::size_t i = 0; i < n_res_tracks; i++) {
-        ASSERT_EQ(res_trk_cands.at(i).header,
-                  legacy_res_trk_cands.at(i).header);
-        ASSERT_EQ(res_trk_cands.at(i).items, legacy_res_trk_cands.at(i).items);
+        ASSERT_EQ(res_trk_cands.at(i), legacy_res_trk_cands.at(i));
     }
 }
