f14b528bd2c728715c00c8033f401deddcda092f
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2026-02-13 15:14:50 +0100

    Removed device::combinatorial_kalman_filter_algorithm::find_tracks_kernel_payload.
    Ended up putting the modified device::find_tracks_payload into its own
    header file to avoid compilation issues arising from the Thrust code use in
    find_tracks.ipp.

--- device/cuda/src/finding/kernels/specializations/find_tracks_src.cuh
+++ device/cuda/src/finding/kernels/specializations/find_tracks_src.cuh
@@ -1,6 +1,6 @@
 /** TRACCC library, part of the ACTS project (R&D line)
  *
- * (c) 2023-2025 CERN for the benefit of the ACTS project
+ * (c) 2023-2026 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -21,8 +21,9 @@ namespace traccc::cuda {
 namespace kernels {
 
 template <typename detector_t>
-__global__ void find_tracks(const finding_config cfg,
-                            device::find_tracks_payload<detector_t> payload) {
+__global__ void find_tracks(finding_config cfg,
+                            typename detector_t::const_view_type det,
+                            device::find_tracks_payload payload) {
     __shared__ unsigned int shared_num_out_params;
     __shared__ unsigned int shared_out_offset;
     __shared__ unsigned int shared_candidates_size;
@@ -36,7 +37,7 @@ __global__ void find_tracks(const finding_config cfg,
     details::thread_id1 thread_id;
 
     device::find_tracks<detector_t>(
-        thread_id, barrier, cfg, payload,
+        thread_id, barrier, cfg, det, payload,
         {shared_num_out_params, shared_out_offset, shared_insertion_mutex,
          shared_candidates, shared_candidates_size});
 }
@@ -46,9 +47,11 @@ __global__ void find_tracks(const finding_config cfg,
 template <typename detector_t>
 void find_tracks(const dim3& grid_size, const dim3& block_size,
                  std::size_t shared_mem_size, const cudaStream_t& stream,
-                 const finding_config cfg,
-                 device::find_tracks_payload<detector_t> payload) {
-    kernels::find_tracks<<<grid_size, block_size, shared_mem_size, stream>>>(
-        cfg, payload);
+                 const finding_config& cfg,
+                 const typename detector_t::const_view_type& det,
+                 const device::find_tracks_payload& payload) {
+
+    kernels::find_tracks<detector_t>
+        <<<grid_size, block_size, shared_mem_size, stream>>>(cfg, det, payload);
 }
 }  // namespace traccc::cuda
